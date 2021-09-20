72125919a0f105dbd39b6ebb2f588fc8647d51d1
Hesz Patrik <heszp01@gmail.com>
2021-09-20 08:41:08 +0200

    1feladat

--- 20210920/20210920/Program.cs
+++ 20210920/20210920/Program.cs
@@ -10,7 +10,22 @@ namespace _20210920
     {
         static void Main(string[] args)
         {
+            Console.Write("Kérek egy számot!");
+            double szam = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+            if(szam % 2 ==0)
+            {
+                Console.WriteLine("A {0} szám páros", szam);
+            }
+            else
+            {
+                Console.WriteLine("A {0} szám páratlan", szam);
+            }
+
+
+
             Console.WriteLine();
+            Console.ReadLine();
         }
     }
 }
