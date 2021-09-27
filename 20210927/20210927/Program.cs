48c0b50455150fc900b6d1a7404485449098855f
Hesz Patrik <heszp01@gmail.com>
2021-09-27 09:39:16 +0200

    12.feladat

---
+++ 20210927/20210927/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _20210927
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Feladat12();
+
+
+
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+
+        static void Feladat12 ()
+        {
+            Console.WriteLine("Mondj egy hónapot!");
+            string honap = Console.ReadLine();
+            switch (honap)
+            {
+                case "1":
+                    Console.WriteLine("január");
+                    break;
+
+                case "2":
+                    Console.WriteLine("február");
+                    break;
+
+                case "3":
+                    Console.WriteLine("március");
+                    break;
+
+                case "4":
+                    Console.WriteLine("április");
+                    break;
+
+                case "5":
+                    Console.WriteLine("május");
+                    break;
+
+                case "6":
+                    Console.WriteLine("junius");
+                    break;
+
+                case "7":
+                    Console.WriteLine("julius");
+                    break;
+
+                case "8":
+                    Console.WriteLine("augusztus");
+                    break;
+
+                case "9":
+                    Console.WriteLine("szeptember");
+                    break;
+
+                case "10":
+                    Console.WriteLine("oktober");
+                    break;
+
+                case "11":
+                    Console.WriteLine("november");
+                    break;
+
+                case "12":
+                    Console.WriteLine("december");
+                    break;
+
+                default:
+                    Console.WriteLine("nincs ilyen honap");
+                    break;
+            }
+        }
+    }
+}
